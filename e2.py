ee40ba9c21583ee6ccd2a6f2fb89c77492c2c662
Beni Contreras <venomsoft@hotmail.com>
2014-10-29 17:29:37 -0600

    Mis ejemplos: Beni Contreras

---
+++ e2.py
@@ -0,0 +1,19 @@
+__author__ = 'venomsoft'
+
+
+def sumaDos():
+    print(10+20)
+
+
+def division(a, b):
+    result = a / b
+    print result
+
+
+def main():
+    sumaDos()
+    division(3.0, 4)
+
+
+if __name__ == "__main__":
+    main()
