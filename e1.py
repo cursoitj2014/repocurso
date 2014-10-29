29ecac937e891bbd80d160287c1d3f2a39dbbf0b
Beni Contreras <venomsoft@hotmail.com>
2014-10-29 17:47:12 -0600

    Mis ejemplos: Beni Contreras 2

--- e1.py
+++ e1.py
@@ -1,1 +1,24 @@
-print "hola mundo"
+class Student(object):
+
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def hola(self):
+        return self.nombre
+
+    def esMayor(self):
+        if self.edad <= 10:
+            return True
+        else:
+            return False
+
+
+def main():
+    e = Student("Benito Contreras", 4)
+    print e.hola()
+    print e.esMayor()
+
+
+if __name__ == "__main__":
+    main()
